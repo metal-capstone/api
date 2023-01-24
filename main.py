341564282979b68f8dda942a1b451c552418166c
Mitchell Sayre <mitchell@vectorone.com>
2023-01-23 22:58:40 -0500

    Add response under root to clarify api is running

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "api is running"}
+
 @app.get("/hello-world")
 async def root():
     return {"message": "Hello World"}
