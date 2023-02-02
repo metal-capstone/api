ae849615e302857f262a3ec2d68351f0f02ed6a6
loganfrey49 <logantfrey@gmail.com>
2023-02-02 14:28:44 -0500

    added redirect to spotify account

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+import string
+import random
+import json
+import requests
 
 app = FastAPI()
 
@@ -16,10 +21,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+creds = json.load(open('credentials.json')) # load in creds from json file
+
+app.scope = "user-read-private user-read-email user-top-read"
+app.state = '' # TODO update api to work with multiple users, this should be per user not global. Still could work with multiple user might be issues if multiple are signing in at the same time
+
 @app.get("/")
 async def root():
     return {"message": "api is running"}
 
-@app.get("/hello-world")
+@app.get("/spotify-login")
 async def root():
-    return {"message": "Hello World"}
+    app.state = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    authorizeLink = 'https://accounts.spotify.com/authorize?response_type=code&client_id=' + creds['spotify_client_id'] + '&scope=' + app.scope + '&redirect_uri=http://localhost:8000/callback&state=' + app.state
+    return RedirectResponse(authorizeLink, status_code=303)
+
+@app.get("/callback")
+async def root(code: str, state: str):
+    if (state != app.state):
+        return {"message": "state_mismatch"}
+    else:
+        app.state = ""
